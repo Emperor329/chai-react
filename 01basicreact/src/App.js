99eabdf40a59d2d4a48cd0a1e745029781cc1fbe
Emperor329 <95495259+Emperor329@users.noreply.github.com>
2023-08-18 17:33:38 +0530

    edited and saved

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>we are ready to go</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  
+
+  return (
+   <h1>we are set to go | narendra </h1>
+  )
+}
+
+export default App
